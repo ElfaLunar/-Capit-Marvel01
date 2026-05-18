65e3a12c965ac5de2e0dfedadc6159bee39dfdde
ElfaLunar <luana.borini@escola.pr.gov.br>
2026-05-18 11:51:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const imageWrapper = document.querySelector('.image-wrapper');
 const infoText = document.getElementById('info-text');
 
 container.addEventListener('mouseenter', () => {
-  imageWrapper.style.left = '65%';
+  imageWrapper.style.left = '75%';
   infoText.style.opacity = '1';
   infoText.style.transform = 'translateX(0)';
 });
 
 container.addEventListener('mouseleave', () => {
-  imageWrapper.style.left = '35%';
+  imageWrapper.style.left = '50%';
   infoText.style.opacity = '0';
   infoText.style.transform = 'translateX(-20px)';
 });
